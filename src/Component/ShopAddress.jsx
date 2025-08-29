fd634beca6d1f9a6b074ad1eafb593c51b008690
Bappa <das_bappa2002@yahoo.co.in>
2025-08-29 18:23:27 +0530

    drop down

--- src/Component/ShopAddress.jsx
+++ src/Component/ShopAddress.jsx
@@ -9,8 +9,31 @@ function ShopAddress() {
 
   const [districtOptions, setDistrictOptions] = useState([]);
   const [subdivisionOptions, setSubOptions] = useState([]);
+  const[policeStationOptions, setPoliceStation]=useState([]);
+  const[postOfficeOptions, setPostOffice]=useState([]);
+   const [ur, setUr] = useState("");
+     const[ruralUrbanOptions, setruralurban]=useState([]);
 
-  
+
+const UrbanArray = [
+    { id: "M",uname: "Municipality"},
+    {id: "NAC",uname: "NAC"},
+    {id: "C",uname: "Corporation"},
+    {id: "CT", uname: "Census Town"}];
+    const RuralArray = [
+    { id: "B",uname: "Block"}];
+
+
+  function onRuralUrban(event) {
+    setUr(event.target.value);
+    //console.log("radio buttion click");
+    //console.log(event.target.value);
+    if(event.target.value=='Urban')
+    setruralurban(UrbanArray);
+    else if(event.target.value=='Rural')
+    setruralurban(RuralArray);
+
+  }
 
   const fetchDistrictOptions = async () => {
     try {
@@ -42,8 +65,39 @@ function ShopAddress() {
 
     try {
       
-       const response = await axios.get(`http://localhost:3000/listapi/getshopSubdivision/${district_id}`);
-      setSubOptions(response.data);
+       const response1 = await axios.get(`http://localhost:3000/listapi/getshopSubdivision/${district_id}`);
+        setSubOptions(response1.data);
+
+      const response2 = await axios.get(`http://localhost:3000/listapi/getshopPoliceStation/${district_id}`);
+        setPoliceStation(response2.data);
+
+       const response3 = await axios.get(`http://localhost:3000/listapi/getshopPostOffice/${district_id}`);
+        setPostOffice(response3.data); 
+      
+    } catch (error) {
+      console.error("Error fetching code options:", error);
+    }
+
+
+  };
+
+  
+
+  const handleBlockMunnicipality = async (event) => {
+   // setDisValue(event.target.value);
+   // const district_id = event.target.value;
+
+    try {
+      
+       const response1 = await axios.get(`http://localhost:3000/listapi/getshopSubdivision/${district_id}`);
+        setSubOptions(response1.data);
+
+     // const response2 = await axios.get(`http://localhost:3000/listapi/getshopPoliceStation/${district_id}`);
+       // setPoliceStation(response2.data);
+
+      // const response3 = await axios.get(`http://localhost:3000/listapi/getshopPostOffice/${district_id}`);
+      //  setPostOffice(response3.data); 
+      
     } catch (error) {
       console.error("Error fetching code options:", error);
     }
@@ -88,6 +142,64 @@ function ShopAddress() {
         
         </label>
           </div>
+
+           <div className="mb-3">
+        <label>
+          Police Station:
+          <select name="policestation"  >
+            <option value="">Select</option>
+            {policeStationOptions.length > 0
+              ? policeStationOptions.map((data) => (
+                  <option key={data.police_station_id} value={data.police_station_id}>
+                    {data.police_station}
+                  </option>
+                ))
+              : "loading police station.."}
+          </select>
+        
+        </label>
+          </div>
+
+          <div className="mb-3">
+        <label>
+          post office:
+          <select name="postoffice"  >
+            <option value="">Select</option>
+            {postOfficeOptions.length > 0
+              ? postOfficeOptions.map((data) => (
+                  <option key={data.post_office_id} value={data.post_office_id}>
+                    {data.post_office}
+                  </option>
+                ))
+              : "loading post office.."}
+          </select>
+        
+        </label>
+          </div>
+           <div onChange={onRuralUrban}>
+      <input type="radio" value="Rural" name="ruralUrban" checked={ur === "Rural"}  /> Rural
+      <input type="radio" value="Urban" name="ruralUrban" checked={ur === "Urban"} /> Urban
+      
+    </div>
+
+       <div className="mb-3">
+        <label>
+          Rural Or Urban:
+          <select name="sruur" onChange={handleBlockMunnicipality} >
+            <option value="">Select</option>
+            {ruralUrbanOptions.length > 0
+              ? ruralUrbanOptions.map((data) => (
+                  <option key={data.id} value={data.uname}>
+                    {data.uname}
+                  </option>
+                ))
+              : "loading post office.."}
+          </select>
+        
+        </label>
+          </div>
+
+
         <button type="submit">Submit</button>
       </form>
       
@@ -96,3 +208,5 @@ function ShopAddress() {
 }
 
 export default ShopAddress
+//https://www.scaler.com/topics/react/react-radio-buttons/
+//https://primereact.org/radiobutton/
